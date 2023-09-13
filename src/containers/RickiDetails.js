967553ab6277673c7aa69589c70a7e6489b02b70
jasminebo <bojasmine08@gmail.com>
2023-09-13 02:25:33 -0500

    preliminary completion of details page

--- src/containers/RickiDetails.js
+++ src/containers/RickiDetails.js
@@ -2,6 +2,41 @@ import React, { Component } from "react";
 import { useParams } from "react-router-dom";
 import { RICKI_API_URL } from "../config";
 import axios from "axios";
+import { Box, CircularProgress, Grid, Typography } from "@mui/material";
+import { withStyles } from "@mui/styles";
+
+const styles = (theme) => ({
+  rickiContainer: {
+    height: "80vh",
+    backgroundColor: "black",
+    color: "white",
+    marginTop: 75,
+    textAlign: "center",
+    borderRadius: 5,
+    paddingTop: 30,
+  },
+  textTitle: {
+    textTransform: "upperCase",
+    fontFamily: "Fantasy",
+  },
+  characterImage: {
+    width: "170px",
+    height: "170px",
+  },
+  infoContainer: {
+    bottom: 60,
+    position: "absolute",
+    backgroundColor: "blue",
+    width: "100%",
+  },
+  separator: {
+    height: "0.01mm",
+    width: "95%",
+  },
+  text: {
+    fontSize: 30,
+  },
+});
 
 function withRouter(Component) {
   function ComponentWithRouter(props) {
@@ -27,12 +62,69 @@ class RickiDetails extends Component {
     });
   }
   render() {
-    return (
-      <div>
-        <h1>Hello</h1>
-      </div>
-    );
+    const { classes } = this.props;
+    const { character } = this.state;
+    if (character) {
+      const { name, image, species, status, location, origin } = character;
+      return (
+        <Box>
+          <Box className={classes.rickiContainer}>
+            <Typography variant="h1" className={classes.textTitle}>
+              {name}
+            </Typography>
+            <img
+              className={classes.characterImage}
+              src={image}
+              alt="character"
+            />
+            <Box className={classes.infoContainer}>
+              <hr className={classes.separator} />
+              <Grid container>
+                <Grid item md={2}>
+                  <Typography className={classes.text}>
+                    Name
+                    <br />
+                    {name}
+                  </Typography>
+                </Grid>
+                <Grid item md={2}>
+                  <Typography className={classes.text}>
+                    Species
+                    <br />
+                    {species}
+                  </Typography>
+                </Grid>
+                <Grid item md={2}>
+                  <Typography className={classes.text}>
+                    Status
+                    <br />
+                    {status}
+                  </Typography>
+                </Grid>
+                <Grid item md={2}>
+                  <Typography className={classes.text}>
+                    Location
+                    <br />
+                    {location.name}
+                  </Typography>
+                </Grid>
+                <Grid item md={2}>
+                  <Typography className={classes.text}>
+                    Origin
+                    <br />
+                    {origin.name}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Box>
+      );
+    } else {
+      return <CircularProgress></CircularProgress>;
+    }
   }
 }
 
-export default withRouter(RickiDetails);
+const detailsPage = withStyles(styles)(RickiDetails);
+export default withRouter(detailsPage);
